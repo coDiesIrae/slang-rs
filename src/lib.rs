ff484387a3aed748ff4efe63825da4c63d1382f2
Nikolay <81352404+coDiesIrae@users.noreply.github.com>
2025-11-17 09:42:59 +0000

    Add force_c_layout compiler option

--- src/lib.rs
+++ src/lib.rs
@@ -802,6 +802,7 @@ impl CompilerOptions {
 	option!(Obfuscate, obfuscate(enable: bool));
 	option!(VulkanUseEntryPointName, vulkan_use_entry_point_name(enable: bool));
 	option!(GLSLForceScalarLayout, glsl_force_scalar_layout(enable: bool));
+	option!(ForceCLayout, force_c_layout(enable: bool));
 	option!(EmitSpirvDirectly, emit_spirv_directly(enable: bool));
 
 	// Debugging
